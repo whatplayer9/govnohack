d5279af96ef215dde522afad84e3d8613fbe1760
whatplayer9 <138493512+whatplayer9@users.noreply.github.com>
2023-12-09 13:58:56 +0300

    Create shopa.lua

---
+++ shopa.lua
@@ -0,0 +1,4 @@
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+local window = library.CreateLib("DmitryHack | Guerra de Torres", "BloodTheme")
+local main = window:NewTab("Combat")
+local section = main:NewSection("Combat")
